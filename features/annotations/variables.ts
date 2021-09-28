3e24a1face3570a211bd3aca0090fa2861316b73
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-28 15:28:24 -0700

    feat: Add array types as examples

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -8,3 +8,15 @@ let nothing: undefined = undefined;
 
 // built in obj
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false, false];
+
+// Classes
+class Car {}
+// var car will only ever refer to an instance of Car
+let car: Car = new Car();
+
+// Object literal
