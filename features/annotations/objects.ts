bf3f210719d193d159659f42dfc667dc0d1a2490
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-28 17:02:59 -0700

    feat: Add object type annotations and desturcting of objects types

---
+++ features/annotations/objects.ts
@@ -0,0 +1,18 @@
+const profile = {
+  name: "alex",
+  age: 20,
+  coords: {
+    lat: 0,
+    lng: 15,
+  },
+  setAge(age: number): void {
+    this.age = age;
+  },
+};
+
+const { age }: { age: number } = profile;
+const {
+  coords: { lat, lng },
+}: { coords: { lat: number; lng: number } } = profile;
+
+// When destructuring must follow the same structure in type annotating.
