c8445380fcc5af557fcaebb307cb8d74c9f17bff
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-29 11:50:39 -0700

    feat: Add google maps class and displays map at center with zoom level 1

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,9 +1,17 @@
 /// <reference types="@types/google.maps" />
 
-import { User, Company } from "./models";
+// import { User, Company } from "./models";
 
-const user = new User();
-console.log(user);
+// const user = new User();
+// console.log(user);
 
-const company = new Company();
-console.log(company);
+// const company = new Company();
+// console.log(company);
+
+new google.maps.Map(<Element>document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
