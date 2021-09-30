7d84a54650ade48df36a2c9426a214de9120d757
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-30 09:50:58 -0700

    load and parse data through map and split

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Hello there");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+//  loads unparsed data as a string
+var matches = fs_1.default
+    .readFileSync("football.csv", {
+    encoding: "utf-8",
+})
+    //  splits data with a new line then maps through them
+    //  and split each point with a comma to create a parsed list
+    .split("\n")
+    .map(function (row) {
+    return row.split(",");
+});
+console.log(matches);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello there");
+import fs from "fs";
+
+//  loads unparsed data as a string
+const matches = fs
+  .readFileSync("football.csv", {
+    encoding: "utf-8",
+  })
+  //  splits data with a new line then maps through them
+  //  and split each point with a comma to create a parsed list
+  .split("\n")
+  .map((row): string[] => {
+    return row.split(",");
+  });
+
+console.log(matches);
