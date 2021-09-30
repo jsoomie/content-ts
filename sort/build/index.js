666b441a1ae9504f0936c29719be3c6f5e76390a
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-29 22:40:06 -0700

    feat: Add linkedlist sort Add list to index to run in terminal

--- sort/build/index.js
+++ sort/build/index.js
@@ -3,13 +3,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
 var charactersCollection_1 = require("./charactersCollection");
+var LinkedList_1 = require("./LinkedList");
+//  Sort numbers
 var numberArray = [10, 3, -5, 0, 8];
 var numbersCollection = new NumbersCollection_1.NumbersCollection(numberArray);
 var numbersSorter = new Sorter_1.Sorter(numbersCollection);
 numbersSorter.sort();
 console.log(numbersCollection.data);
+//  Sort characters
 var characterString = "XaatvAYzbk";
 var charactersCollection = new charactersCollection_1.CharacterCollection(characterString);
 var charaterSorter = new Sorter_1.Sorter(charactersCollection);
 charaterSorter.sort();
 console.log(charactersCollection.data);
+//  Sort LinkedList
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+var linkSorter = new Sorter_1.Sorter(linkedList);
+linkSorter.sort();
+linkedList.print();
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,15 +1,29 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharacterCollection } from "./charactersCollection";
+import { LinkedList } from "./LinkedList";
 
+//  Sort numbers
 const numberArray = [10, 3, -5, 0, 8];
 const numbersCollection = new NumbersCollection(numberArray);
 const numbersSorter = new Sorter(numbersCollection);
 numbersSorter.sort();
 console.log(numbersCollection.data);
 
+//  Sort characters
 const characterString = "XaatvAYzbk";
 const charactersCollection = new CharacterCollection(characterString);
 const charaterSorter = new Sorter(charactersCollection);
 charaterSorter.sort();
 console.log(charactersCollection.data);
+
+//  Sort LinkedList
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const linkSorter = new Sorter(linkedList);
+linkSorter.sort();
+linkedList.print();
