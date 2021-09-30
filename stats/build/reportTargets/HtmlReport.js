c49932e9e3ce1db64a809360d1b3cd8661542fd6
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-30 16:41:04 -0700

    refactor: Change htmlreports to a full html writeup

--- stats/build/reportTargets/HtmlReport.js
+++ stats/build/reportTargets/HtmlReport.js
@@ -9,7 +9,7 @@ var HtmlReports = /** @class */ (function () {
     function HtmlReports() {
     }
     HtmlReports.prototype.print = function (report) {
-        var html = "\n      <div>\n        <h1>Analysis Output</h1>\n        <div>" + report + "</div>\n      </div>\n    ";
+        var html = "\n  <!DOCTYPE html>\n  <html lang=\"en\">\n  <head>\n    <meta charset=\"UTF-8\">\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    <title>Report</title>\n  </head>\n  <body>\n    <div>\n      <h1>Analysis Output</h1>\n      <div>" + report + "</div>\n    </div>\n    <footer>  \n      <p>This Report was created by a class inside!</p>\n    </footer>\n  </body>\n  </html>\n    ";
         fs_1.default.writeFileSync("report.html", html);
     };
     return HtmlReports;
--- stats/src/reportTargets/HtmlReport.ts
+++ stats/src/reportTargets/HtmlReport.ts
@@ -4,10 +4,24 @@ import fs from "fs";
 export class HtmlReports implements OutputTarget {
   print(report: string): void {
     const html = `
-      <div>
-        <h1>Analysis Output</h1>
-        <div>${report}</div>
-      </div>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Report</title>
+  </head>
+  <body>
+    <div>
+      <h1>Analysis Output</h1>
+      <div>${report}</div>
+    </div>
+    <footer>  
+      <p>This Report was created by a class inside!</p>
+    </footer>
+  </body>
+  </html>
     `;
 
     fs.writeFileSync("report.html", html);
