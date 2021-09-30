c7be91bd12aae2a4e04243c5be9d0cb51736e338
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-29 21:30:06 -0700

    feat: Add charactersCollection class to be sorted

--- sort/build/index.js
+++ sort/build/index.js
@@ -2,8 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
+var charactersCollection_1 = require("./charactersCollection");
 var numberArray = [10, 3, -5, 0, 8];
 var numbersCollection = new NumbersCollection_1.NumbersCollection(numberArray);
-var sorter = new Sorter_1.Sorter(numbersCollection);
-sorter.sort();
+var numbersSorter = new Sorter_1.Sorter(numbersCollection);
+numbersSorter.sort();
 console.log(numbersCollection.data);
+var characterString = "XaatvAYzbk";
+var charactersCollection = new charactersCollection_1.CharacterCollection(characterString);
+var charaterSorter = new Sorter_1.Sorter(charactersCollection);
+charaterSorter.sort();
+console.log(charactersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,15 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharacterCollection } from "./charactersCollection";
 
 const numberArray = [10, 3, -5, 0, 8];
 const numbersCollection = new NumbersCollection(numberArray);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const numbersSorter = new Sorter(numbersCollection);
+numbersSorter.sort();
 console.log(numbersCollection.data);
+
+const characterString = "XaatvAYzbk";
+const charactersCollection = new CharacterCollection(characterString);
+const charaterSorter = new Sorter(charactersCollection);
+charaterSorter.sort();
+console.log(charactersCollection.data);
