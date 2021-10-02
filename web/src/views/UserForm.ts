a4b9af62d559f71446fc598f39c7e418b8dd6a16
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-02 09:35:58 -0700

    feat: Add bindings to click events and hover events

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,19 +1,46 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      "click:button": this.onButtonClick,
+      "mouseenter:h1": this.onHeaderHover,
+    };
+  }
+
+  onButtonClick(): void {
+    console.log("Hello there");
+  }
+
+  onHeaderHover(): void {
+    console.log("H1 hovered!");
+  }
+
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
         <input></input>
+        <button>Click Me</button>
       </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement("template");
     if (templateElement) {
       templateElement.innerHTML = this.template();
+      this.bindEvents(templateElement.content);
       this.parent.append(templateElement.content);
     }
   }
