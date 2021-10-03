e302850940e996fc330887a85d599a9aaf782a8f
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-03 08:15:59 -0700

    docs: update comments in home controllers explaining home route

--- server/src/controllers/home.controllers.ts
+++ server/src/controllers/home.controllers.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
 
+/**
+ * Home page that says that the user is either logged in or not
+ * @param req req.session from logged in user
+ * @param res sends out html with either logged in or tell them to login
+ */
 export const home = (req: Request, res: Response) => {
   try {
     if (req.session && req.session.loggedIn) {
