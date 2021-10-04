46deb16c44fc99c66cca4ec834859c81fe0b9aa6
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-04 15:29:05 -0700

    refactor: Add methods to metadata get

--- server_refactor/src/controllers/decorators/routes.ts
+++ server_refactor/src/controllers/decorators/routes.ts
@@ -1,7 +1,17 @@
 import "reflect-metadata";
+import { Methods } from "./Methods";
 
-export function get(path: string) {
-  return function (target: any, key: string, desc: PropertyDescriptor) {
-    Reflect.defineMetadata("path", path, target, key);
+function routeBinder(method: string) {
+  return function (path: string) {
+    return function (target: any, key: string, desc: PropertyDescriptor) {
+      Reflect.defineMetadata("path", path, target, key);
+      Reflect.defineMetadata("method", method, target, key);
+    };
   };
 }
+
+export const get = routeBinder(Methods.GET);
+export const put = routeBinder(Methods.PUT);
+export const post = routeBinder(Methods.POST);
+export const del = routeBinder(Methods.DEL);
+export const patch = routeBinder(Methods.PATCH);
