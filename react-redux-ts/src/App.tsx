a649e0198aedaba0a1fce83017a7e40b6212371a
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-05 08:21:55 -0700

    feat: add redux and create a dummy reducer for now

--- react-redux-ts/src/App.tsx
+++ react-redux-ts/src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import { Home } from "./pages";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { reducers } from "./reducers";
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <Home />
+      <Provider store={store}>
+        <Home />
+      </Provider>
     </div>
   );
 }
--- react-redux-ts/src/components/Work.tsx
+++ react-redux-ts/src/components/Work.tsx
@@ -1,25 +1,8 @@
-import { useState } from "react";
-
-export const Work = (): JSX.Element => {
-  const numberState = 0;
-  const [count, setCount] = useState(numberState);
-
-  const increment = (): void => {
-    setCount((prev) => prev + 1);
-  };
-
-  const decrement = (): void => {
-    setCount((prev) => prev - 1);
-  };
-
-  return (
+import React from "react";
+export class Work extends React.Component {
+  return() {
     <div>
-      <h1>Work file</h1>
-      <h1>{count}</h1>
-
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={() => setCount(numberState)}>Reset</button>
-    </div>
-  );
-};
+      <h1>Hello there!</h1>
+    </div>;
+  }
+}
