7bb644bae88e89d0019049b2e546edcea5d73edb
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-05 08:50:06 -0700

    feat: create action types fetch todo for now

--- react-redux-ts/src/actions/index.ts
+++ react-redux-ts/src/actions/index.ts
@@ -1,14 +1,21 @@
 // ACTIONS
 import axios from "axios";
 import { Dispatch } from "redux";
+import { Action } from "./types";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 const URL = "https://jsonplaceholder.typicode.com/todos";
 
 export const fetchTodos = () => {
   return async (dispatch: Dispatch) => {
-    const res = await axios.get(URL);
+    const res = await axios.get<Todo[]>(URL);
     dispatch({
-      type: "FETCH_TODOS",
+      type: Action.FETCH_TODOS,
       payload: res.data,
     });
   };
