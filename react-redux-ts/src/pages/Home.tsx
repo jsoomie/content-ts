09e2b0c39167d584e98781b0f1a1ec33867bb8df
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-04 20:37:55 -0700

    feat: create demo of simple increment and decrement

--- react-redux-ts/src/pages/Home.tsx
+++ react-redux-ts/src/pages/Home.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
+const defaultState = 0;
+
 export const Home = () => {
+  const [count, setCount] = useState(defaultState);
+
+  const increment = (): void => {
+    setCount((prev) => prev + 1);
+  };
+
+  const decrement = (): void => {
+    setCount((prev) => prev - 1);
+  };
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>{count}</h1>
+
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={() => setCount(defaultState)}>Reset</button>
     </div>
   );
 };
