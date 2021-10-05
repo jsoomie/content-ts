eabaf486bec5e1bd0bc9c8d15f9eabf81baa1de6
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-04 19:08:00 -0700

    feat: create defintion descriptor for routehandler

--- server_refactor/src/controllers/decorators/routes.ts
+++ server_refactor/src/controllers/decorators/routes.ts
@@ -1,10 +1,15 @@
 import "reflect-metadata";
 import { Methods } from "./Methods";
 import { MetaDataKeys } from "./MetaDataKeys";
+import { RequestHandler } from "express";
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 function routeBinder(method: string) {
   return function (path: string) {
-    return function (target: any, key: string, desc: PropertyDescriptor) {
+    return function (target: any, key: string, desc: RouteHandlerDescriptor) {
       Reflect.defineMetadata(MetaDataKeys.PATH, path, target, key);
       Reflect.defineMetadata(MetaDataKeys.METHOD, method, target, key);
     };
