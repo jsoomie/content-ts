71b3b8846de57ecbeda98dba4fc6429f12d60c1b
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-05 21:00:16 -0700

    chore: create interface for main fetch reducer

--- react-redux-ts/src/reducers/index.ts
+++ react-redux-ts/src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from "redux";
+import { todosReducer } from "./todos";
+import { Todo } from "../actions";
 
-export const reducers = combineReducers({
-  counter: (prev: number) => prev + 1,
+export interface StoreState {
+  todos: Todo[];
+}
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer,
 });
