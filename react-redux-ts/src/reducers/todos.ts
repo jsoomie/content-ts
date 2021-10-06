a4587ba2e0bf2076c679be4ecbf23eb2e76466a3
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-06 09:36:23 -0700

    feat: create delete action in reducer

--- react-redux-ts/src/reducers/todos.ts
+++ react-redux-ts/src/reducers/todos.ts
@@ -4,6 +4,8 @@ export const todosReducer = (state: Todo[] = [], action: ActionType) => {
   switch (action.type) {
     case Action.FETCH_TODOS:
       return action.payload;
+    case Action.DELETE_TODO:
+      return state.filter((todo) => todo.id !== action.payload);
     default:
       return state;
   }
