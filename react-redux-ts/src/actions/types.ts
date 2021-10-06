94330effce6c1b52691eaa05b9bbc2fb3043bc9b
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-06 09:32:56 -0700

    refactor: create action type to use in todosreducer to reduce size for future reference always add on more types in types.ts

--- react-redux-ts/src/actions/types.ts
+++ react-redux-ts/src/actions/types.ts
@@ -1,4 +1,8 @@
+import { FetchTodosAction, DeleteTodoAction } from ".";
+
 export enum Action {
   FETCH_TODOS,
   DELETE_TODO,
 }
+
+export type ActionType = FetchTodosAction | DeleteTodoAction;
--- react-redux-ts/src/reducers/todos.ts
+++ react-redux-ts/src/reducers/todos.ts
@@ -1,6 +1,6 @@
-import { Todo, FetchTodosAction, Action } from "../actions";
+import { Todo, ActionType, Action } from "../actions";
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: ActionType) => {
   switch (action.type) {
     case Action.FETCH_TODOS:
       return action.payload;
