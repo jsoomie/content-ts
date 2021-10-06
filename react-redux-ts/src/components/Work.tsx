28bf35fd4a83df455248983800b16d52bc2116b9
Jonathan Soukaseume <sksmejn@gmail.com>
2021-10-05 21:15:20 -0700

    chore: connect states togetherin work.tsx to return

--- react-redux-ts/src/components/Work.tsx
+++ react-redux-ts/src/components/Work.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-export class Work extends React.Component {
+import { connect } from "react-redux";
+import { Todo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers";
+
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+export class _App extends React.Component<AppProps> {
   return() {
     <div>
       <h1>Hello there!</h1>
     </div>;
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos: todos };
+};
+
+export const Work = connect(mapStateToProps, { fetchTodos })(_App);
